3af8abf43757eb8d060a682926a0b1bb67d5008f
roji-tech <rojitech9@gmail.com>
2025-04-23 11:05:47 +0100

    todo app done

--- projects/todoapp/src/components/Todos/counter.style.js
+++ projects/todoapp/src/components/Todos/counter.style.js
@@ -1,7 +1,95 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const TodosWrapper = styled.div`
-   
-`
+  .loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 2rem;
+    color: #333;
+  }
+
+  .addTodo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px 0;
+
+    input {
+      margin: 5px 0;
+      padding: 10px;
+      width: 300px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+    input[type="checkbox"] {
+      width: 20px;
+      height: 20px;
+    }
+    input[type="text"] {
+      width: min(400px, 100%);
+    }
+    input[type="text"]:focus {
+      border-color: #007bff;
+      outline: none;
+    }
+    input[type="checkbox"]:checked {
+      background-color: #007bff;
+    }
+    input[type="checkbox"]:checked:focus {
+      outline: none;
+    }
+    input[type="checkbox"]:checked:hover {
+      background-color: #0056b3;
+    }
+    input[type="text"]:hover {
+      border-color: #0056b3;
+    }
+  }
+
+  .todos {
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+    padding: 20px;
+
+    .todo {
+      border-bottom: 1px solid #ccc;
+      padding: 10px 0;
+
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .todo h2 {
+      margin: 0;
+    }
+
+    .todo p {
+      margin-top: 5px;
+    }
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  button {
+    display: block;
+    margin: 20px auto;
+    padding: 10px 20px;
+    font-size: 1rem;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #0056b3;
+  }
+`;
 
 // export default CounterWrapper
--- projects/todoapp/src/components/Todos/index.jsx
+++ projects/todoapp/src/components/Todos/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { TodosWrapper } from "./counter.style";
 import { Link } from "react-router-dom";
 
@@ -30,7 +30,15 @@ const reducer = (state, action) => {
       break;
 
     case ACTIONS.TOGGLE_LOADING:
-      return (state.loading = !state.loading);
+      return {
+        ...state,
+        loading: !state.loading,
+      };
+    case ACTIONS.FETCH_TODOS:
+      return {
+        ...state,
+        todos: action.payload,
+      };
 
     default:
       break;
@@ -41,15 +49,226 @@ const Todos = ({ isLoggedIn }) => {
   const [state, dispatch] = useReducer(reducer, initialData);
   const user = JSON.parse(sessionStorage.getItem("user"));
 
-  return isLoggedIn ? (
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [completed, setcompleted] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+
+  const fetchTodos = async () => {
+    dispatch({ type: ACTIONS.TOGGLE_LOADING });
+    console.log(
+      "Fetching todos...",
+      "http://localhost:3000/todos?userId=" + user.id
+    );
+
+    const response = await fetch(
+      "http://localhost:3000/todos?userId=" + user.id
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      dispatch({ type: ACTIONS.TOGGLE_LOADING });
+      console.error("Error fetching todos:", response.statusText);
+      return;
+    }
+
+    if (data.length === 0) {
+      dispatch({ type: ACTIONS.TOGGLE_LOADING });
+      console.log("No todos found");
+      return;
+    }
+
+    console.log("Todos", data);
+
+    dispatch({ type: ACTIONS.FETCH_TODOS, payload: data });
+    dispatch({ type: ACTIONS.TOGGLE_LOADING });
+  };
+
+  const addTodo = async () => {
+    if (!title || !description) {
+      console.error("Title and description are required");
+      return;
+    }
+
+    const response = await fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        completed,
+        userId: user.id,
+      }),
+    });
+
+    if (!response.ok) {
+      console.error("Error creating todo:", response.statusText);
+
+      return;
+    }
+
+    const data = await response.json();
+    console.log("Todo created:", data);
+    fetchTodos();
+    alert("Todo created successfully");
+  };
+
+  const deleteTodo = async (id) => {
+    const response = await fetch("http://localhost:3000/todos/" + id, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      console.error("Error deleting todo:", response.statusText);
+      return;
+    }
+
+    fetchTodos();
+  };
+
+  const markTodo = async (id) => {
+    const todo = state.todos.find((todo) => todo.id === id);
+    const response = await fetch("http://localhost:3000/todos/" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        completed: !todo.completed,
+      }),
+    });
+
+    if (!response.ok) {
+      console.error("Error marking todo:", response.statusText);
+      return;
+    }
+
+    fetchTodos();
+  };
+
+  const stageForEdit = (todo) => {
+    setCurrentId(todo.id);
+    setTitle(todo.title);
+    setDescription(todo.description);
+    setcompleted(todo.completed);
+    console.log("Staging for edit:", todo);
+  };
+
+  const editTodo = async () => {
+    console.log("Editing todo:", currentId);
+    const response = await fetch("http://localhost:3000/todos/" + currentId, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        completed,
+      }),
+    });
+    if (!response.ok) {
+      console.error("Error editing todo:", response.statusText);
+      return;
+    }
+    const data = await response.json();
+    console.log("Todo edited:", data);
+    fetchTodos();
+
+    clearForm();
+    alert("Todo edited successfully");
+  };
+
+  const clearForm = () => {
+    setCurrentId(null);
+    setTitle("");
+    setDescription("");
+    setcompleted(false);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
     <TodosWrapper>
-      <h1>Welcome {user.username}</h1>
+      {!isLoggedIn ? (
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      ) : (
+        <div>
+          {state.loading ? (
+            <div className="loading">
+              <p>Loading...</p>
+            </div>
+          ) : (
+            <>
+              <h1>Todos</h1>
+
+              <h1>Add Todo</h1>
+              <div className="addTodo">
+                <input
+                  type="text"
+                  placeholder="Title"
+                  id="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Description"
+                  id="description"
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                />
+                <div>
+                  <label htmlFor="completed">Completed</label> &nbsp;
+                  <input
+                    type="checkbox"
+                    id="completed"
+                    onChange={(e) => setcompleted(e.target.checked)}
+                    checked={completed}
+                  />
+                </div>
+
+                <div className="submit">
+                  <button onClick={currentId ? editTodo : addTodo}>
+                    {currentId ? "Edit Todo" : "Add Todo"}
+                  </button>
+
+                  <button onClick={clearForm}>Clear Form</button>
+                </div>
+              </div>
+
+              <div className="todos">
+                {state.todos?.map((todo) => (
+                  <div key={todo.id} className="todo">
+                    <h2>{todo.title}</h2>
+                    <p>{todo.description}</p>
+
+                    <div className="actions">
+                      <button onClick={() => deleteTodo(todo.id)}>
+                        Delete
+                      </button>
+
+                      <button onClick={() => markTodo(todo.id)}>
+                        {todo.completed ? "Undone" : "Done"}
+                      </button>
+
+                      <button onClick={() => stageForEdit(todo)}>Edit</button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      )}
     </TodosWrapper>
-  ) : (
-    <div>
-      <h1>Please Login</h1>
-      <Link to="/">Login</Link>
-    </div>
   );
 };
 
